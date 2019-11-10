5611a265e8cc9c11b80e198d7dba7b22fe3af11b
Jenkins <skreker99@gmail.com>
2019-11-10 23:22:46 +0200

    Version 2.0

--- RPS.py
+++ RPS.py
@@ -23,7 +23,7 @@ UserName="User"
 UserName2="Comp"
 Score1=0
 Score2=0
-
+result=''
 
 
 
@@ -37,26 +37,38 @@ def companswer():
     return computer
 
 
-def answer(player):
+def answer(player): 
+    global Score1,Score2
     computer= companswer()
     if player == computer:
         result="Tie!"
     elif player == 1:
         if computer == 2:
             result="You lose! Paper covers Rock"
+            Score2+=1
         else:
             result="You win!  Rock smashes Scissors"
+            Score1+=1
     elif player == 2:
         if computer == 3:
             result="You lose! Scissors cut Paper"
+            Score2+=1
         else:
             result="You win! Paper covers Rock"
+            Score1+=1
     elif player == 3:
         if computer == 1:
             result="You lose! Rock smashes Scissors"
+            Score2+=1
         else:
-             result="You win! Scissors cut Paper"
+            result="You win! Scissors cut Paper"
+            Score1+=1
     print(result)
+    
+
+    canvas.itemconfig(res, text=result)
+    canvas.itemconfig(s1, text=Score1)
+    canvas.itemconfig(s2, text=Score2)
 
     print(player)
     print(computer)
@@ -87,17 +99,14 @@ ScissorsButton=canvas.create_image(288, 584, image=scissors, anchor=NW)
 
 canvas.create_text(180, 255, fill = '#f3f3f3',font=("Impact", 50),text=UserName, anchor=NW)
 canvas.create_text(771, 255, fill = '#f3f3f3',font=("Impact", 50),text=UserName2, anchor=NW)
-canvas.create_text(232, 144, fill = '#f3f3f3',font=("Impact", 60),text=Score1, anchor=NW)
-canvas.create_text(839, 144, fill = '#f3f3f3',font=("Impact", 60),text=Score2, anchor=NW)
-
-
+s1=canvas.create_text(232, 144, fill = '#f3f3f3',font=("Impact", 60),text=Score1, anchor=NW)
+s2=canvas.create_text(839, 144, fill = '#f3f3f3',font=("Impact", 60),text=Score2, anchor=NW)
+res=canvas.create_text(540, 611, justify='center',fill = '#f3f3f3',font=("Impact", 25),text=result, anchor=NW)
 
 canvas.tag_bind(RockButton, "<Button-1>", clicked1)
 canvas.tag_bind(PaperkButton, "<Button-1>", clicked2)
 canvas.tag_bind(ScissorsButton, "<Button-1>", clicked3)
 canvas.pack()
 
-# b = Button(root, text="My Text")
-# b.place(x=297, y=23)
 
 root.mainloop()
